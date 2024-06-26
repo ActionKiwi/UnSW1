751d0f9a07bad214fbc3d84cb5970f6dac939848
ActionKiwi <benjaminaldridge6@gmail.com>
2024-06-26 12:45:40 +1200

    Started on modal and did navigation links.

--- src/routes/sim/+page.svelte
+++ src/routes/sim/+page.svelte
@@ -1,17 +1,17 @@
 <body>
     <nav>
         <div id="left_navigation">
-            <button class="navigation">
-                Simulation
-            </button>
-            <button class="navigation">
-                Teams
-            </button>
+            <div class="navigation">
+                <a href="/sim">Simulation</a>
+            </div>
+            <div class="navigation">
+                <a href="/team">Teams</a>
+            </div>
         </div>
         <div id="right_navigation">
-            <button id="profile">
-                Profile
-            </button>
+            <div id="profile">
+                <!-- <a href="/profile">Profile</a> -->
+            </div>
         </div>
     </nav>
     <div id="container">
@@ -31,23 +31,66 @@
         <div id="treset">
             <div class="select_team">
                 <button id="pop_up" on:click={unhide_modal(team1)}>Select Team 1</button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
             </div>
             <div class="select_team">
                 <button id="pop_up" on:click={unhide_modal(team2)}>Select Team 2</button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
-                <button class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team2, pokemon, index)}></button>
             </div>
-            <button id="reset_button">Reset</button>
+            <button>Reset</button>
+        </div>
+    </div>
+    <div id="teamselectModal">
+        <div class="selectpokemonLeft">
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
+        </div>
+        <div class="selectpokemonRight">
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
+            <button disabled class="select_pokemon_from_team hiddenbyDefault" on:click={starting_position(team1, pokemon, index)}></button>
         </div>
     </div>
 </body>
+
+
+<style>
+
+nav {
+    display: flex;
+    flex-direction: row;
+} 
+
+#container {
+    display: flex;
+    flex-direction: column;
+}
+
+#sim_screen {
+    display: flex;
+    flex-direction: row;
+}
+
+#manualareachange_container {
+    display: flex;
+    flex-direction: column;
+}
+
+#treset {
+    display: flex;
+    flex-direction: row;
+}
+
+.select_team {
+    display: flex;
+    flex-direction: column;
+}
+
+.selectpokemonLeft {
+    justify-content: space-evenly;
+}
+
+.selectpokemonRight {
+    justify-content: space-evenly;
+}
+
+</style>
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,28 +1,28 @@
 <body>
     <nav>
         <div id="left_navigation">
-            <button class="navigation">
-                Simulation
-            </button>
-            <button class="navigation">
-                Teams
-            </button>
+            <div class="navigation">
+                <a href="/sim">Simulation</a>
+            </div>
+            <div class="navigation">
+                <a href="/team">Teams</a>
+            </div>
         </div>
         <div id="right_navigation">
-            <button id="profile">
-                Profile
-            </button>
+            <div id="profile">
+                <a href="/profile">Profile</a>
+            </div>
         </div>
     </nav>
     <div id="container">
         <div class="team">
             <p>Team name goes here</p>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
-            <button class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(createTeam)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
+            <button disabled class="add_pokemon_to_team hidebyDefault" on:click={unhidemodal(openPokemon)}></button>
         </div>
         <button id="new_team">+ Create a new team</button>
     </div>
